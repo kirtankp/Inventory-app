19ef62210a80187495fd6070dfca3552c5ab1b76
kirtan <kirtan1902@gmail.com>
2023-11-02 11:35:34 +0530

    otp template added to  login

--- src/app/page.js
+++ src/app/page.js
@@ -12,12 +12,15 @@ export default function Home() {
     email: ''
   });
 
+  const [showOtpForm, setShowOtpForm] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = async (e) => {
+    setShowOtpForm(true);
     e.preventDefault();
     // Handle login logic here (e.g., send data to the server)
     try {
@@ -37,33 +40,41 @@ export default function Home() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2 className="text-2xl mb-4">Log In</h2>
-      <div className="mb-4">
-        <label>
-          Role: 
-          <select className='w-28 text-center m-2 py-1 rounded' name="role" value={formData.role} onChange={handleChange}>
-            <option value="store">Store</option>
-            <option value="employee">Employee</option>
-          </select>
-        </label>
-      </div>
-      <div className="mb-4">
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-          value={formData.email}
-          className="w-full px-3 py-2 border rounded"
-        />
-      </div>
-      <button
-        type="submit"
-        className="mb-4 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-      >
-        Generate OTP
-      </button>
-    </form>
+    !showOtpForm ? (
+      <form onSubmit={handleSubmit}>
+        <h2 className="text-2xl mb-4">Log In</h2>
+        <div className="mb-4">
+          <label>
+            Role:
+            <select className='w-28 text-center m-2 py-1 rounded' name="role" value={formData.role} onChange={handleChange}>
+              <option value="store">Store</option>
+              <option value="employee">Employee</option>
+            </select>
+          </label>
+        </div>
+        <div className="mb-4">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={formData.email}
+            className="w-full px-3 py-2 border rounded"
+          />
+        </div>
+        <button
+          type="submit"
+          className="mb-4 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+        >
+          Generate OTP
+        </button>
+      </form>
+    ) : (
+      <>
+        <p>Enter OTP sent to your email:</p>
+        <input type="text" placeholder="OTP" className="w-full p-2 my-2" />
+        <button className="w-full bg-blue-500 text-white p-2 rounded my-2">Submit</button>
+      </>
+    )
   )
 }
